910367d915f77dc3ec762016ebf628b8309b3290
notbadwt <notbad_wangt@126.com>
2016-03-04 09:59:25 +0800

    修改团购订单付款成功后的状态

--- ef-website/src/main/java/com/efeiyi/ec/website/order/controller/PurchaseOrderPaymentController.java
+++ ef-website/src/main/java/com/efeiyi/ec/website/order/controller/PurchaseOrderPaymentController.java
@@ -89,7 +89,7 @@ public class PurchaseOrderPaymentController {
                 if (purchaseOrder.getOrderType() != null && purchaseOrder.getOrderType().equals("3")) {  //礼品卷
                     orderStatus = PurchaseOrder.ORDER_STATUS_WRGIFT;
                 } else if (purchaseOrder.getOrderType() != null && purchaseOrder.getOrderType().equals("4")) {  //团购
-                    orderStatus = PurchaseOrder.ORDER_STATUS_WAIT_GROUP;
+                    orderStatus = "5";
                 } else {
                     orderStatus = PurchaseOrder.ORDER_STATUS_WRECEIVE;
                 }
